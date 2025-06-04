9fda9ae0902c5829476c67418ecaa7728687526f
Shivam12-ui558 <shivamkumar12082004@gmail.com>
2025-06-04 21:10:59 +0530

    second commit in Node.js

--- server.js
+++ server.js
@@ -2,35 +2,24 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-
-
+const PORT = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
   res.send('Hello! Welcome to Our Hotel')
 })
 
 
-
-
-
-
 // import the router files
 const personRoutes = require('./routes/personRoutes');
-
+const menuItem = require('./routes/menuItemRoutes');
 // use the Routers
 app.use('/person', personRoutes);
-
-// import the router files
-const menuItem = require('./routes/menuItemRoutes');
-
-// use the Routers
 app.use('/MenuItem', menuItem);
-
-
 
 
 app.listen(3000, () => {
